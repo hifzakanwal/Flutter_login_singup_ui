cb746af7a92622726fe71e80434f1a652f8f6726
Hifza <hifzakanwal75@gmail.com>
2022-09-19 12:40:31 -0700

    Login_Screen_UI

---
+++ lib/loginscreen.dart
@@ -0,0 +1,158 @@
+import 'dart:ui';
+
+import 'package:firebase_auth/Component.dart';
+import 'package:flutter/material.dart';
+
+class loginscreen extends StatefulWidget {
+  const loginscreen({super.key});
+
+  @override
+  State<loginscreen> createState() => _loginscreenState();
+}
+
+class _loginscreenState extends State<loginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    final emailcon = TextEditingController();
+    final Passwordcon = TextEditingController();
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(15, 187, 187, 187),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            decoration: BoxDecoration(
+                color: bcolor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                Center(
+                  child: Text(
+                    "Firebase",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Container(
+                    width: MediaQuery.of(context).size.height * 0.4,
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24),
+                          ),
+                          SizedBox(
+                            height: 45,
+                          ),
+                          TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              controller: emailcon,
+                              decoration: InputDecoration(
+                                hintText: "Email",
+                              )),
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: Passwordcon,
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Forgot password?",
+                                    style: TextStyle(color: Colors.black),
+                                  )),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 55,
+                          ),
+                          roundbutton(title: "Login", tapfun: () {}),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("- OR -"),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 37,
+                            width: 49,
+                            decoration: BoxDecoration(
+                              color: bcolor,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(
+                              Icons.phone,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  TextButton(onPressed: () {}, child: Text("Sign Up")),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'loginscreen.dart';
+import 'Component.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff2470c7),
+      ),
+      home:
+          loginscreen(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
