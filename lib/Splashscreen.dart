b068d15bce47e563033d145f73049cebaf33e1cb
Hifza <hifzakanwal75@gmail.com>
2022-09-20 04:55:03 -0700

    Splash_Screen_UI

---
+++ lib/Splashscreen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:lottie/lottie.dart';
+
+import 'package:firebase_auth/loginscreen.dart';
+import 'package:flutter/material.dart';
+
+class splashscreen extends StatefulWidget {
+  const splashscreen({super.key});
+
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(Duration(seconds: 5), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => loginscreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Lottie.network(
+              'https://assets1.lottiefiles.com/packages/lf20_s3zjvwxq.json',
+              width: 200,
+              height: 200,
+              animate: true)),
+    );
+  }
+}
